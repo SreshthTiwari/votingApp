3fcf05773432379a0210db783d593b2bd91a0eb7
Sreshth <sreshthtiwari13@gmail.com>
2024-06-29 20:53:43 +0530

    structure

---
+++ votingAppDir/src/Components/firstPage/firstPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function firstPage(props){
+    return(
+        <>
+        
+            <h1>first page</h1>
+
+        </>
+    )
+}
---
+++ votingAppDir/src/Components/votingForm/votingForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function votingForm(props){
+    return(
+        <>
+        
+            <h1>Voting Form</h1>
+
+        </>
+    )
+}
