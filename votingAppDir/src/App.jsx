435e53f5f3124b29331620dfefeb662f5e9113c2
Sreshth Tiwari <83489627+SreshthTiwari@users.noreply.github.com>
2024-07-02 15:55:03 +0530

    hide firebase key

--- votingAppDir/src/App.jsx
+++ votingAppDir/src/App.jsx
@@ -5,12 +5,6 @@ import { getAuth } from "firebase/auth"
 import studentCard from "./Components/studentCard/studentCard.jsx"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpVqoOIpnrVU5j3n2R5tWhVI_Rr6QnMW4",
-  authDomain: "votingapp-be942.firebaseapp.com",
-  projectId: "votingapp-be942",
-  storageBucket: "votingapp-be942.appspot.com",
-  messagingSenderId: "885556007289",
-  appId: "1:885556007289:web:277b264917f224ab0ddfa8"
 };
 
 const app = initializeApp(firebaseConfig);
